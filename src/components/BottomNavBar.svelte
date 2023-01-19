10138ac115c0be281eb2cca30fb8c7f45f5a45c9
Carlos <carlos@carlosmoshu.com>
2023-01-19 08:50:45 +0100

    Add bottom-bar to the layout

---
+++ src/components/BottomNavBar.svelte
@@ -0,0 +1,82 @@
+<script>
+
+</script>
+
+<svelte:head>
+    <meta name="description" content="bottom-bar" />
+</svelte:head>
+
+<section>
+    <ul id="nav-buttons">
+        <li>
+            <a href="/">
+                <i class="fas fa-home"></i>Home
+            </a>
+        </li>
+        <li>
+            <a href="/alerts">
+                <i class="fas fa-exclamation-triangle"></i>Alerts
+            </a>
+        </li>
+        <li>
+            <a href="/devices">
+                <i class="fas fa-server"></i>Devices
+            </a>
+        </li>
+        <li>
+            <a href="/support">
+                <i class="fas fa-question-circle"></i>Support
+            </a>
+        </li>
+        
+    </ul>
+</section>
+
+<style>
+    /* Import fa icons */
+    @import url('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css');
+    section {
+        background-color: #1e1e1e;
+        color: #fff;
+        font-size: 1rem;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+        position: fixed;
+        left: -4vw;
+        bottom: 0;
+        width: 104%;
+        height: 5vh;
+    }
+    ul {
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+        width: 100%;
+        height: 100%;
+        list-style: none;
+    }
+    li {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+        width: 100%;
+        height: 100%;
+        border-top: 1px solid #ccc8;
+        border-right: 1px solid #ccc8;
+        border-left: 1px solid #ccc8;
+        opacity: 0.8;
+    }
+
+    a {
+        text-decoration: none;
+        color: #fff;
+        font-size: 1.2rem;
+    }
+    i {
+        margin: 0 1vw;
+    }
+
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,53 +1,22 @@
 <script>
-	import Header from './Header.svelte';
+	import BottomNavBar from './../components/BottomNavBar.svelte';
 	import './styles.css';
 </script>
 
 <div class="app">
-	<Header />
-
 	<main>
 		<slot />
 	</main>
-
 	<footer>
-		<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
+		<BottomNavBar />
 	</footer>
 </div>
 
 <style>
-	.app {
-		display: flex;
-		flex-direction: column;
-		min-height: 100vh;
-	}
-
-	main {
-		flex: 1;
-		display: flex;
-		flex-direction: column;
-		padding: 1rem;
-		width: 100%;
-		max-width: 64rem;
-		margin: 0 auto;
-		box-sizing: border-box;
-	}
-
+	/* Footer sticky at the bottom */
 	footer {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		padding: 12px;
-	}
-
-	footer a {
-		font-weight: bold;
-	}
-
-	@media (min-width: 480px) {
-		footer {
-			padding: 12px 0;
-		}
+		position: fixed;
+		bottom: 0;
+		width: 100%;
 	}
 </style>
