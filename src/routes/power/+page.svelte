2d03540a5e13bd081c3652f9a8cffb3377746366
Carlos <carlos@carlosmoshu.com>
2023-01-20 10:11:45 +0100

    Add reboot option

---
+++ src/routes/power/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+
+</script>
+
+<svelte:head>
+	<meta name="description" content="Main-Zabbix" />
+</svelte:head>
+
+<section>
+
+</section>
+
+<style>
+
+</style>
---
+++ src/routes/reboot/+page.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	const { exec } = require('child_process');
+	exec('sudo reboot now', (error: any, stdout: any, stderr: any) => {
+		if (error) {
+			console.log(`error: ${error.message}`);
+			return;
+		}
+		if (stderr) {
+			console.log(`stderr: ${stderr}`);
+			return;
+		}
+		console.log(`stdout: ${stdout}`);
+	});
+	
+</script>
+
+<svelte:head>
+	<meta name="description" content="Main-Zabbix" />
+</svelte:head>
+
+<section>
+
+</section>
+
+<style>
+
+</style>
